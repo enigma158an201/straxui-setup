0178a69465be49c35409b8a111c086ef52b7a474
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-15 17:32:13 +0200

    1st improvements for disable IPv6 section

--- switch-from-iptables-to-nftables-setup-sky.sh
+++ switch-from-iptables-to-nftables-setup-sky.sh
@@ -20,6 +20,17 @@ set -euxo pipefail
 # bridge 			ebtables 
 
 # myifname=enp10s0 # enp4s0
+function grubUpdate() {
+	if [ -x /usr/sbin/update-grub ]; then sudo update-grub
+	elif [ -x /usr/sbin/grub2-mkconfig ]; then sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+	elif [ -x /usr/sbin/grub-mkconfig ]; then sudo grub-mkconfig -o /boot/grub/grub.cfg
+	fi
+}
+function getNetworkManagement() {
+	if [ -f /etc/netplan/* ]; then
+		grep -i renderer /etc/netplan/*
+	fi
+}
 function restore-nft-conf () {
 	mynftconfdst=/etc/nftables.conf
 	mynftconfsrc=".$mynftconfdst"
@@ -29,8 +40,7 @@ function restore-nft-conf () {
 		sudo install -o root -g root -m 0744 -pv "$mynftconfsrc" "$mynftconfdst"
 	else
 		echo "$isErrorFree"
-		return 1
-		exit
+		exit 1 # return 1
 	fi
 }
 function blacklist-iptables-kernel-modules {
@@ -41,13 +51,40 @@ function blacklist-iptables-kernel-modules {
 function blacklist-ip6-kernel-modules {
 	myip6bckldst="/etc/sysctl.d/00-disable-ip6-R13.conf"
 	myip6bcklsrc=".$myip6bckldst"
+	sudo mkdir -p /etc/sysctl.d/
 	sudo install -o root -g root -m 0744 -pv "$myip6bcklsrc" "$myip6bckldst"
+	#todo check if include /etc/systctl.d present -> not necessary
+	#sudo update-initramfs -u
+	#todo check if already added
+	bDisabledIpV6="$(grep ^GRUB_CMDLINE_LINUX /etc/default/grub | grep ipv6.disable || echo "false")"
+	if [ "$bDisabledIpV6" = "false" ]; then 
+		sudo sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub
+		grubUpdate
+	fi
 }
+function blacklist-ip6-NetworkManagement() {
+	#non persistant, but take effect immediately
+	sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+	sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+	sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+	sudo sysctl -p
 
+	if [ -x /usr/bin/nmcli ] && (systemctl status NetworkManager); then
+		# all=$(LC_ALL=C nmcli dev status | tail -n +2); first=${all%% *}; echo "$first"
+		for ConnectionName in $(LC_ALL=C nmcli dev status | tail -n +2 | grep -Eo '^[^ ]+'); do # $(nmcli connection show | awk '{ print $1 }') 
+			sudo nmcli connection modify "$ConnectionName" ipv6.method "disabled" || true # be careful with connection names including spaces
+		done
+	fi
+	if (systemctl status systemd-networkd); then
+		#sed -i '/[Network]/ s/"$/nLinkLocalAddressing=ipv4"/' /etc/systemd/networkd.conf; fi
+		if (! grep '^LinkLocalAddressing=ipv4' /etc/systemd/networkd.conf); then sudo sed -i '/^\[Network\].*/a LinkLocalAddressing=ipv4 ' /etc/systemd/networkd.conf ;fi 
+	fi
+}
 function mainDisableIptablesIp6 {
 	restore-nft-conf
 	blacklist-iptables-kernel-modules
 	blacklist-ip6-kernel-modules
+	blacklist-ip6-NetworkManagement
 	sudo apt install nftables
 	echo "  >>> Remise à zéro des règles chargées en mémoire avant basculement iptables vers nftables"
 	sudo iptables -F
@@ -62,6 +99,7 @@ function mainDisableIptablesIp6 {
 	#restore-nft-conf
 	sudo systemctl enable --now nftables
 	sudo systemctl restart NetworkManager
+
 
 	if [ -x /usr/sbin/iptables-nft ]; then sudo update-alternatives --set iptables /usr/sbin/iptables-nft; fi
 	if [ -x /usr/sbin/ip6tables-nft ]; then sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-nft; fi
