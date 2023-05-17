d47a6f26d5eceb75a2c0d6ccd25afa8722ef2f3d
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-17 20:49:40 +0200

    replace install deps by general include

--- update-or-install-strax-wallet-deb-bullseye.sh
+++ update-or-install-strax-wallet-deb-bullseye.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # https://github.com/stratisproject/StraxUI.git
+bash ./apt-pre-instal-pkg-ubuntu.sh
 
 function determinerOS() {
 	cat /etc/os-release # echo `lsb_release -a`
@@ -72,16 +73,16 @@ isDebianLike="$isUbuntuLike$isDebian$isDeepin"
 # read -rp "Mettre à jour Strax o/N" -n 1 upgradeStratis
 # if [ ! "${upgradeStratis^^}" = "N" ] && [ ! "$upgradeStratis" = "" ]; then
 	if [ ! "$isDebianLike" = "" ]; then
-		sudo apt install git jq curl
+		#sudo apt-get install -y git jq curl
 		#libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
 		if [ ! "$isBuster" = "" ]; then
-			sudo apt install libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
+			sudo apt-get install -y libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
 			projectlatestcontentdeb=$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[0] | .browser_download_url')
 			myfilenamedeb=$(basename "$projectlatestcontentdeb")
 			if [ ! -f "$myfilenamedeb" ]; then wget "$projectlatestcontentdeb"; fi
 			sudo dpkg -i "$myfilenamedeb"
 		elif [ "$isBuster" = "" ]; then
-			sudo apt install libappindicator3-0.1-cil{,-dev}
+			sudo apt-get install -y libappindicator3-0.1-cil{,-dev}
 			straxuidestfolder=/opt/straxui
 			projectlatestcontentgz=$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[1] | .browser_download_url')
 			myfilenamegz=$(basename "$projectlatestcontentgz")
