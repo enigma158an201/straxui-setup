65f5b982f4e6c618bb3b9d9365c4e65a5726aa63
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-19 13:24:16 +0200

    add uncomment & setParameterInFile functions

--- include/file-edition.sh
+++ include/file-edition.sh
@@ -1,8 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail #; set -x
+launchDir="$(dirname "$0")"
+if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
+source "${launchDir}/include/test-superuser-privileges.sh"
+
 comment() {
 	local regex="${1:?}"
 	local file="${2:?}"
 	local comment_mark="${3:-#}"
 	if [ -f "$file" ]; then suExecCommand sed -ri "s:^([ ]*)($regex):\\1$comment_mark\\2:" "$file"; fi
+}
+uncomment() {
+	local regex="${1:?}"
+	local file="${2:?}"
+	local comment_mark="${3:-#}"
+	if [ -f "$file" ]; then suExecCommand sed -ri "s:^([ ]*)[$comment_mark]+[ ]?([ ]*$regex):\\1\\2:" "$file"; fi
 }
 appendLineAtEnd() {
 	local newLine="${1:?}"
@@ -20,4 +33,26 @@ insertLineAfter() {
 	local newLine="${2:?}"
 	local file="${3:?}"
 	if [ -f "$file" ]; then suExecCommand sed -ri "/^([ ]*)($regex)/a $newLine" "$file"; fi
+}
+setParameterInFile() {
+	# 2 cas de figures: 1/ le parametre est present et il faut le remplacer 2/ le parametre n'est pas présent, il sera ajouté à la fin
+	local inputfile="$1"
+	local findText="$2"
+	local setnewparam="$3"
+
+	for s in "|" "#" "/" ":" ";" "~"; do 
+		if [ "$(grep "$s" <<< "$findText")" = "" ]; then 		separateursed="$"; break; fi
+	done
+	if [ "$(grep -i "$setnewparam" "$inputfile")" = "" ]; then	isAlreadySet="false"
+	else														isAlreadySet="true"
+	fi
+	if [ "$isAlreadySet" = "false" ]; then
+		if [ "$(grep -i "$findText" "$inputfile")" = "" ]; then	ispresent="false"
+		else													ispresent="true"
+		fi
+		if [ "$ispresent" = "true" ]; then						cmdarg="s$separateursed.*$findText.*$separateursed$setnewparam$separateursed""g";	
+																suExecCommand sed -Ei_old "$cmdarg" "$inputfile" # 'g' "$inputfile" # | tee "$inputfile" -
+		else													echo "$setnewparam" | $sPfxSu tee -a "$inputfile" -
+		fi
+	fi
 }
