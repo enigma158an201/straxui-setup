7abe1d888e0f584b5fdd70a188afd0515a7c3bdb
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-19 10:52:38 +0200

    update include paths

--- include/pre-install-pkgs.sh
+++ include/pre-install-pkgs.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 #pwd
 
 aptPreinstall() {
-	if [ -x /usr/bin/apt-get ]; then	suExecCommand "bash ${launchDir}/apt-pre-instal-pkg-ubuntu.sh"
+	if [ -x /usr/bin/apt-get ]; then	suExecCommand "bash ${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
 	else 								exit 1
 	fi
 }
@@ -13,7 +13,7 @@ main() {
 	launchDir="$(dirname "$0")"
 	if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
 	source "${launchDir}/include/test-superuser-privileges.sh"
-	suExecCommand "install -o root -g root -m 0755 -pv $launchDir/test-superuser-privileges.sh /usr/local/bin/su-alternatives-exec"
+	suExecCommand "install -o root -g root -m 0755 -pv $launchDir/include/test-superuser-privileges.sh /usr/local/bin/su-alternatives-exec"
 	aptPreinstall
 }
 main
