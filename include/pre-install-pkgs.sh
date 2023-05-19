b77e14d0be4e1048211aee021868d8f5f7fafe52
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-19 12:42:58 +0200

    replace euxo by euo and move launchdir directive

--- include/pre-install-pkgs.sh
+++ include/pre-install-pkgs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail #; set -x
 
 #pwd
+launchDir="$(dirname "$0")"
+if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
 
 aptPreinstall() {
 	if [ -x /usr/bin/apt-get ]; then	suExecCommand "bash ${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
@@ -10,8 +12,6 @@ aptPreinstall() {
 }
 
 main() {
-	launchDir="$(dirname "$0")"
-	if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
 	source "${launchDir}/include/test-superuser-privileges.sh"
 	suExecCommand "install -o root -g root -m 0755 -pv $launchDir/include/test-superuser-privileges.sh /usr/local/bin/su-alternatives-exec"
 	aptPreinstall
