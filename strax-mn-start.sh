5b662dda8c4c23ebe892b4951baade0decd38a28
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 15:58:40 +0200

    add check for path to screen dotnet python3

--- strax-mn-start.sh
+++ strax-mn-start.sh
@@ -7,5 +7,6 @@ if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
 source "${launchDir}/include/test-superuser-privileges.sh"
 #source "${launchDir}/include/file-edition.sh"
 
-suExecCommand screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet
-suExecCommand python3 ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py
+# to do add check for paths screen + dotnet + python3
+if (which dotnet 1>/dev/null) && (which screen 1>/dev/null); then                               suExecCommand screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet; fi
+if (which python3 1>/dev/null) && [ -f ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py ]; then   suExecCommand python3 ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py; fi
