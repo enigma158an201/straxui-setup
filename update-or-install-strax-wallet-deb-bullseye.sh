baac8e20037fc5ff428b786766f7dcdd07387896
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 19:28:38 +0200

    add main & installStrax function & dlDir variable

--- update-or-install-strax-wallet-deb-bullseye.sh
+++ update-or-install-strax-wallet-deb-bullseye.sh
@@ -78,28 +78,37 @@ fi
 isDeepin=$(determineSiDeepin)
 isDebianLike="$isUbuntuLike$isDebian$isDeepin"
 
-# read -rp "Mettre à jour Strax o/N" -n 1 upgradeStratis
-# if [ ! "${upgradeStratis^^}" = "N" ] && [ ! "$upgradeStratis" = "" ]; then
-	if [ ! "$isDebianLike" = "" ]; then
-		#sudo apt-get install -y git jq curl
-		#libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
-		if [ ! "$isBuster" = "" ]; then
-			suExecCommand "apt-get install -y libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2"
-			projectlatestcontentdeb=$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[0] | .browser_download_url')
-			myfilenamedeb=$(basename "$projectlatestcontentdeb")
-			if [ ! -f "$myfilenamedeb" ]; then wget "$projectlatestcontentdeb"; fi
-			suExecCommandNoPreserveEnv "dpkg -i $myfilenamedeb"
-		elif [ "$isBuster" = "" ]; then
-			suExecCommand "apt-get install -y libappindicator3-0.1-cil{,-dev}"
-			projectlatestcontentgz=$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[1] | .browser_download_url')
-			myfilenamegz=$(basename "$projectlatestcontentgz")
-			if [ ! -f "$myfilenamegz" ]; then wget "$projectlatestcontentgz"; fi			
-			
-			suExecCommand "folderinsidetar=$(tar --exclude=\"*/*\" -tf \"\$myfilenamegz\"); straxuidestfolder=/opt/straxui/; mkdir -p \$straxuidestfolder; \ 
-			tar --strip-components=1 -C \"\$straxuidestfolder" -xvzf "\$myfilenamegz\" \"\$folderinsidetar" # /opt/straxui
-		fi
+installStrax() {
+	# read -rp "Mettre à jour Strax o/N" -n 1 upgradeStratis
+	# if [ ! "${upgradeStratis^^}" = "N" ] && [ ! "$upgradeStratis" = "" ]; then
 
-		# echo $projectlatestcontent
-		
-	fi
-# fi
+		if [ ! "$isDebianLike" = "" ]; then
+			dlDir="/tmp/"
+			#sudo apt-get install -y git jq curl
+			#libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
+			if [ ! "$isBuster" = "" ]; then
+				suExecCommand "apt-get install -y libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2"
+				projectlatestcontentdeb="$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[0] | .browser_download_url')"
+				myfilenamedeb="${dlDir}$(basename "$projectlatestcontentdeb")"
+				if [ ! -f "$myfilenamedeb" ]; then wget -O "$myfilenamedeb" "$projectlatestcontentdeb"; fi
+				suExecCommandNoPreserveEnv "dpkg -i $myfilenamedeb"
+			elif [ "$isBuster" = "" ]; then
+				suExecCommand "apt-get install -y libappindicator3-0.1-cil{,-dev}"
+				projectlatestcontentgz="$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[1] | .browser_download_url')"
+				myfilenamegz="${dlDir}$(basename "$projectlatestcontentgz")"
+				if [ ! -f "$myfilenamegz" ]; then wget -O "$myfilenamegz" "$projectlatestcontentgz"; fi			
+				
+				suExecCommand "folderinsidetar=$(tar --exclude=\"*/*\" -tf \"\$myfilenamegz\"); straxuidestfolder=/opt/straxui/; mkdir -p \$straxuidestfolder; \ 
+				tar --strip-components=1 -C \"\$straxuidestfolder" -xvzf "\$myfilenamegz\" \"\$folderinsidetar" # /opt/straxui
+			fi
+
+			# echo $projectlatestcontent
+			
+		fi
+	# fi
+}
+
+main() {
+	installStrax
+}
+main
