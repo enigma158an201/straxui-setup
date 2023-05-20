623e400d35e2c33ae590669e8a21a3a0874fcfd6
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 21:05:29 +0200

    optimize calls to suExecCommand

--- include/set-common-settings.sh
+++ include/set-common-settings.sh
@@ -42,12 +42,12 @@ cronjob-disable-ipv6() {
     if (systemctl status cron.service); then suExecCommand systemctl enable --now cron.service; fi
 }
 main() {
-	source "${launchDir}/include/test-superuser-privileges.sh"
-	source "${launchDir}/include/file-edition.sh"
-	sshd-config-settings
-    disable-wifi-connections
-    disable-cups-services
-	disable-systemd-sleep
-    cronjob-disable-ipv6
+	suExecCommand "source \"${launchDir}/include/test-superuser-privileges.sh\"; \
+	source \"${launchDir}/include/file-edition.sh\"; \
+	sshd-config-settings; \
+    disable-wifi-connections; \
+    disable-cups-services; \
+	disable-systemd-sleep; \
+    cronjob-disable-ipv6"
 }
 main
