49b5fe3d75d8f732fab801682cb9a2b2356c8c77
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 11:26:47 +0200

    add unbloat function

--- include/apt-pre-instal-pkg-ubuntu.sh
+++ include/apt-pre-instal-pkg-ubuntu.sh
@@ -1,14 +1,39 @@
 #!/usr/bin/env bash
 set -euo pipefail #; set -x
 
-main() {
+getDpkgListInstalled() {
+	#dpkg -l | grep -E '(^|\s+)cron\b'
+	dpkgGlobList="$(dpkg -l)"
+	prefix="ii"
+	dpkgInstalled="${dpkgGlobList[@]//$prefix/}"
+	echo "${dpkgInstalled}"
+}
+
+aptPreinstallPkg() {
 	declare -a pkgsToInstall
-	pkgsToInstall=(net-tools wget curl tar zip ipv6calc git jq xinit)
+	pkgsToInstall=(net-tools wget curl tar zip ipv6calc git jq xinit cron)
 	apt-get update && apt-get upgrade
 	for pkgToInstall in "${pkgsToInstall[@]}" #${pkgsToInstall[*]}
 	do
-		apt-get -y install "$pkgToInstall"
+		if true; then
+			apt-get -y install "$pkgToInstall"
+		fi
 	done
 }
-
+aptUnbloatPkg() {
+	declare -a pkgsToRemove
+	pkgsToRemove=(sane-utils )
+	apt-get update && apt-get upgrade
+	for pkgsToRemove in "${pkgsToRemove[@]}" #${pkgsToRemove[*]}
+	do
+		if true; then
+			apt-get -y autoremove "$pkgsToRemove"
+		fi
+	done
+}
+main() {
+	getDpkgListInstalled
+	#aptPreinstallPkg
+	#aptUnbloatPkg
+}
 main
