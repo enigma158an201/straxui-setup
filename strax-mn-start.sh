136ca575d01a433f6e2219bf7d3f4854eb02da7e
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 16:07:29 +0200

    add to do comment

--- strax-mn-start.sh
+++ strax-mn-start.sh
@@ -7,6 +7,7 @@ if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
 source "${launchDir}/include/test-superuser-privileges.sh"
 #source "${launchDir}/include/file-edition.sh"
 
-# to do add check for paths screen + dotnet + python3
-if (which dotnet 1>/dev/null) && (which screen 1>/dev/null); then                               suExecCommand screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet; fi
-if (which python3 1>/dev/null) && [ -f ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py ]; then   suExecCommand python3 ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py; fi
+if (which dotnet 1>/dev/null) && (which screen 1>/dev/null); then								suExecCommand screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet; fi
+if (which python3 1>/dev/null) && [ -f ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py ]; then	suExecCommand python3 ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py; fi
+
+# to do: migrate or import some variables
