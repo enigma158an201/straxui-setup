579b8b10788dc8393adcf183c22ee6985667b618
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-21 00:03:50 +0200

    ajust dest path for node

--- strax-mn-start.sh
+++ strax-mn-start.sh
@@ -6,8 +6,8 @@ launchDir="$(dirname "$0")"
 if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"
 source "${launchDir}/include/test-superuser-privileges.sh"
 #source "${launchDir}/include/file-edition.sh"
-
-if (which dotnet 1>/dev/null) && (which screen 1>/dev/null); then								suExecCommand screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet; fi
-if (which python3 1>/dev/null) && [ -f ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py ]; then	suExecCommand python3 ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py; fi
-
+suExecCommandNoPreserveEnv "
+if (which dotnet 1>/dev/null) && (which screen 1>/dev/null); then								screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet; fi
+if (which python3 1>/dev/null) && [ -f ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py ]; then	python3 ~/StraxCLI/StraxCLI-StraxCLI-1.0.0/straxcli.py; fi
+"
 # to do: migrate or import some variables
