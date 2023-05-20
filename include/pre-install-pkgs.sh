f5ff99487857fc832b2ef4bf9bcbe200b8b86b26
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 20:09:09 +0200

    add include/apt-pre-instal-pkg-ubuntu.sh

--- include/pre-install-pkgs.sh
+++ include/pre-install-pkgs.sh
@@ -4,6 +4,8 @@ set -euo pipefail #; set -x
 #pwd
 launchDir="$(dirname "$0")"
 if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi
+source "${launchDir}/include/test-superuser-privileges.sh"
+source "${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
 
 aptPreinstall() {
 	if [ -x /usr/bin/apt-get ]; then	suExecCommand "source ${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh; aptPreinstallPkg; aptUnbloatPkg"
@@ -12,7 +14,7 @@ aptPreinstall() {
 }
 
 main() {
-	source "${launchDir}/include/test-superuser-privileges.sh"
+	
 	suExecCommand "install -o root -g root -m 0755 -pv $launchDir/include/test-superuser-privileges.sh /usr/local/bin/su-alternatives-exec"
 	aptPreinstall
 }
