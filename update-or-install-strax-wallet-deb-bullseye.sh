43de6fa6ba528d80966d5a9b30224ef2eb673948
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-20 19:37:31 +0200

    add check before install pkg in installStrax

--- update-or-install-strax-wallet-deb-bullseye.sh
+++ update-or-install-strax-wallet-deb-bullseye.sh
@@ -87,13 +87,24 @@ installStrax() {
 			#sudo apt-get install -y git jq curl
 			#libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
 			if [ ! "$isBuster" = "" ]; then
-				suExecCommand "apt-get install -y libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2"
+				
+				suExecCommand "pkgsToInstall=(libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2); \
+				for pkgsToInstall in \$pkgsToInstall; do \
+					if [ $(checkDpkgInstalled \"\$pkgToInstall\") = \"false\" ]; then
+						apt-get install -y \"\$pkgsToInstall\"
+					fi
+				done"
 				projectlatestcontentdeb="$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[0] | .browser_download_url')"
 				myfilenamedeb="${dlDir}$(basename "$projectlatestcontentdeb")"
 				if [ ! -f "$myfilenamedeb" ]; then wget -O "$myfilenamedeb" "$projectlatestcontentdeb"; fi
 				suExecCommandNoPreserveEnv "dpkg -i $myfilenamedeb"
 			elif [ "$isBuster" = "" ]; then
-				suExecCommand "apt-get install -y libappindicator3-0.1-cil{,-dev}"
+				suExecCommand "pkgsToInstall=(libappindicator3-0.1-cil{,-dev}); \
+				for pkgsToInstall in \$pkgsToInstall; do \
+					if [ $(checkDpkgInstalled \"\$pkgToInstall\") = \"false\" ]; then
+						apt-get install -y \"\$pkgsToInstall\"
+					fi
+				done"
 				projectlatestcontentgz="$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[1] | .browser_download_url')"
 				myfilenamegz="${dlDir}$(basename "$projectlatestcontentgz")"
 				if [ ! -f "$myfilenamegz" ]; then wget -O "$myfilenamegz" "$projectlatestcontentgz"; fi			
