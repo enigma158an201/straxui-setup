5df9bd80cf01fbdbd36ca3b7cf0f87a7957b04c9
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-21 18:43:14 +0200

    comment useless include line

--- update-or-install-strax-wallet-deb-bullseye.sh
+++ update-or-install-strax-wallet-deb-bullseye.sh
@@ -9,7 +9,7 @@ if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi; launchDir="${launchDir//
 source "${launchDir}/include/pre-install-pkgs.sh"
 source "${launchDir}/include/test-superuser-privileges.sh"
 #source "${launchDir}/include/file-edition.sh"
-source "${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
+#source "${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
 source "${launchDir}/include/set-common-settings.sh"
 
 function determinerOS() {
