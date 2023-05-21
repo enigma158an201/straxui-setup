a2211dc7f19db1932d15036ee9884549a19ea482
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-21 19:07:10 +0200

    replace apt-get by /usr/bin/apt-get

--- update-or-install-strax-wallet-deb-bullseye.sh
+++ update-or-install-strax-wallet-deb-bullseye.sh
@@ -89,7 +89,7 @@ main_installStrax() {
 
 		if [ ! "$isDebianLike" = "" ]; then
 			dlDir="/tmp/"
-			#sudo apt-get install -y git jq curl
+			#sudo /usr/bin/apt-get install -y git jq curl
 			#libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libappindicator3-1 libsecret-1-0 libasound2
 			echo -e "/t>>> check and/or install straxui deps"
 			if [ ! "$isBuster" = "" ]; then
@@ -98,7 +98,7 @@ main_installStrax() {
 				for pkgsToInstall in \$pkgsToInstall; do \
 					isInstalled=\$(checkDpkgInstalled \"\$pkgToInstall\"); \
 					if [ \"\$isInstalled\" = \"false\" ]; then \
-						apt-get install -y \$pkgsToInstall; \
+						/usr/bin/apt-get install -y \$pkgsToInstall; \
 					fi; \
 				done\""
 				projectlatestcontentdeb="$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[0] | .browser_download_url')"
@@ -112,7 +112,7 @@ main_installStrax() {
 				for pkgsToInstall in \$pkgsToInstall; do \
 					isInstalled=\$(checkDpkgInstalled \"\$pkgToInstall\"); \
 					if [ \"\$isInstalled\" = \"false\" ]; then \
-						apt-get install -y \$pkgsToInstall; \
+						/usr/bin/apt-get install -y \$pkgsToInstall; \
 					fi; \
 				done\""
 				projectlatestcontentgz="$(curl -s https://api.github.com/repos/stratisproject/StraxUI/releases/latest | jq -r '.assets[1] | .browser_download_url')"
