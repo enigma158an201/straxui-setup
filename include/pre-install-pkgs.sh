0e608f83c05fde00c4ef36f245bfce4910d12835
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-21 18:42:18 +0200

    add verbose echo messages

--- include/pre-install-pkgs.sh
+++ include/pre-install-pkgs.sh
@@ -5,16 +5,17 @@ set -euo pipefail #; set -x
 launchDir="$(dirname "$0")"
 if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"
 source "${launchDir}/include/test-superuser-privileges.sh"
-source "${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
+#source "${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh"
 
 aptPreinstall() {
-	if [ -x /usr/bin/apt-get ]; then	suExecCommand "source ${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh; aptPreinstallPkg; aptUnbloatPkg"
+	if [ -x /usr/bin/apt-get ]; then	echo -e "/t>>> add deps packages for the script to run correctly"
+										suExecCommand "source ${launchDir}/include/apt-pre-instal-pkg-ubuntu.sh; aptPreinstallPkg; aptUnbloatPkg"
 	else 								exit 1
 	fi
 }
 
 main_aptPreinstall() {
-	
+	echo -e "/t>>> add script test-superuser-privileges.sh to /usr/local/bin/su-alternatives-exec location"
 	suExecCommand "install -o root -g root -m 0755 -pv $launchDir/include/test-superuser-privileges.sh /usr/local/bin/su-alternatives-exec"
 	aptPreinstall
 }
