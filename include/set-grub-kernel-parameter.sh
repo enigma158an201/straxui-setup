6e314b4391efd481f602c8ba4b323c44683215d1
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-21 22:02:38 +0200

    removed variables scommand

--- include/set-grub-kernel-parameter.sh
+++ include/set-grub-kernel-parameter.sh
@@ -9,11 +9,14 @@ grubUpdate() {
 	fi
 }
 main_grubsettings() {
-    sCommand="sed -i '/GRUB_CMDLINE_LINUX/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub"
-    sCommandDefault="sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub"
-	#echo -e "$sCommand \n $sCommandDefault"
-	$sCommand
-    $sCommandDefault 			#sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub then #sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ ipv6.disable=1"/' /etc/default/grub
+    #sCommand="sed -i '/GRUB_CMDLINE_LINUX/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub"
+    #sCommandDefault="sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub"
+	##echo -e "$sCommand \n $sCommandDefault"
+	#$sCommand
+    #$sCommandDefault 			#sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub then #sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ ipv6.disable=1"/' /etc/default/grub
+	sed -i '/GRUB_CMDLINE_LINUX/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub
+    sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub
+	
 	grubUpdate
 }
 main_grubsettings
