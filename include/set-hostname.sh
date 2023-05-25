45b97e7de9bbfba54dca9e97dd1b12d72d32c68b
enigma158an201 <gwenael.goujon@gmail.com>
2023-05-25 12:57:20 +0200

    add precisions for systemd (todo manage openrc and other usecase)

--- include/set-hostname.sh
+++ include/set-hostname.sh
@@ -5,17 +5,23 @@ set -euo pipefail #; set -x
 launchDir="$(dirname "$0")"
 if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"
 source "${launchDir}/include/test-superuser-privileges.sh"
+systemdHostnameFile=/etc/hostname
+
+getSystemdHostnameFileContent() {
+	cat "$systemdHostnameFile"
+}
 
 main_set_hostname() {
  	#source ${launchDir}/include/test-superuser-privileges.sh
- 	hostnameFile=/etc/hostname
  	hostsFile=/etc/hosts
  	sHardwareModel=$(/usr/sbin/dmidecode -s system-product-name)
  	sOsIdLine=$(grep -i '^ID=' /etc/os-release)
  	sOsId=${sOsIdLine//ID=/}
- 	sOldHostname=$(cat $hostnameFile)
+	#if systemd
+ 	sOldHostname="$(getSystemdHostnameFileContent)" #$(cat $hostnameFile)
  	sNewHostname=${sHardwareModel,,}-${sOsId}
- 	if [ ! "${sOldHostname}" = "${sNewHostname}" ]; then  sed -i.old s/"${sOldHostname}"/"${sNewHostname}"/g "${hostnameFile}"; fi
+ 	#replace old hostname by new one if needed
+	if [ ! "${sOldHostname}" = "${sNewHostname}" ]; then  sed -i.old s/"${sOldHostname}"/"${sNewHostname}"/g "${hostnameFile}"; fi
  	if (grep "${sOldHostname}" "${hostsFile}"); then 	  sed -i.old s/"${sOldHostname}"/"${sNewHostname}"/g "${hostsFile}"; fi
 }
 main_set_hostname
