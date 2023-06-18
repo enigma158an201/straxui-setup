e555377fa7ec485bfecabdf972f350f1ad6d68b9
enigma158an201 <gwenael.goujon@gmail.com>
2023-06-18 13:23:12 +0200

    update update-sources-list.sh to be more verbose

--- update-sources-list.sh
+++ update-sources-list.sh
@@ -9,13 +9,16 @@ if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; fi; launchDir="${launchDir//
 source "${launchDir}/include/test-superuser-privileges.sh"
 
 
+
 getInstalledDebianDistCodeName() {
 	myetcosrelease=/etc/os-release
 	if [ -f "${myetcosrelease}" ]; then
 		sOsId="$(grep -i ^ID= "${myetcosrelease}")"
+		sOsId="${sOsId##=*}"
 		if [ "$sOsId" = "debian" ]; then 
 			sOsRelease="$(grep -i ^VERSION_CODENAME= "${myetcosrelease}")"
-			sOsRelease="${sOsRelease##*}"
+			sOsRelease="${sOsRelease##=*}"
+			echo "${sOsRelease}"
 		else
 			exit 2
 		fi
@@ -27,21 +30,36 @@ sCurrentDistCodename="$(getInstalledDebianDistCodeName)"
 
 installNewSourcesList() {
 	mysourceslistdst="/etc/apt/sources.list"
-	if [ "$sCurrentDistCodename" = "buster" ]; then			mysourceslistsrc="${launchDir}etc/apt/bullseye-sources.list"
-	elif [ "$sCurrentDistCodename" = "bullseye" ]; then		mysourceslistsrc="${launchDir}etc/apt/bookworm-sources.list"
-	elif [ "$sCurrentDistCodename" = "bookworm" ]; then		mysourceslistsrc="${launchDir}etc/apt/bookworm-sources.list"
+	if [ "${sCurrentDistCodename}" = "buster" ]; then			mysourceslistsrc="${launchDir}etc/apt/bullseye-sources.list"
+	elif [ "${sCurrentDistCodename}" = "bullseye" ]; then		mysourceslistsrc="${launchDir}etc/apt/bookworm-sources.list"
+	elif [ "${sCurrentDistCodename}" = "bookworm" ]; then		mysourceslistsrc="${launchDir}etc/apt/bookworm-sources.list"
 	fi
 	mysourceslistbak="${mysourceslistdst}.${sCurrentDistCodename}-gg.save"
-	echo -e "\t>>> sauvegarde du fichier ${mysourceslistdst} buster dans ${mysourceslistbak}"
-	suExecCommand mv -i "$mysourceslistdst" "$mysourceslistbak"	
-	echo -e "\t>>> installation de la version suivante pour ${mysourceslistdst} "
-	suExecCommand install -o root -g root -m 0744 -pv "$mysourceslistsrc" "$mysourceslistdst"
+	diff "${mysourceslistsrc}" "${mysourceslistdst}"
+	echo -e "\t>>> Avant d'installer la nouvelle version, s'assurer d'avoir bien appliqué les dernieres mises à jour avec \`apt update && apt upgrade\`"
+	read -rp " Confirmer la nouvelle version? (o/N)" -n 1 confirmOverwriteRepo
+	if [ "${confirmOverwriteRepo^^}" = "O" ]; then
+		echo -e "\t>>> sauvegarde du fichier ${mysourceslistdst} pour Debian ${sCurrentDistCodename} dans ${mysourceslistbak}"
+		if [ -f "${mysourceslistdst}" ]; then 
+			suExecCommand mv -i "${mysourceslistdst}" "${mysourceslistbak}"	
+		fi
+		echo -e "\t>>> installation de la version suivante pour ${mysourceslistdst}"
+		suExecCommand install -o root -g root -m 0744 -pv "${mysourceslistsrc}" "${mysourceslistdst}"
+		if true; then 
+			echo -e "\t>>> nouvelle version de ${mysourceslistdst} installee avec succès \
+			\n\t>>> pour installer les nouveaux paquets, lancer en root (avec sudo ou su - ET sans les crochets) :\
+			\n \t [sudo] apt update && [sudo] apt dist-upgrade "
+		fi
+	else
+		echo -e "\t>>> abandon de l'installation de la version suivante pour ${mysourceslistsrc}"
+		exit 1
+	fi
 	unset mysourceslist{dst,src,bak}
 }
 
 main() {
-	#if [ "$sCurrentDistCodename" = "buster" ]; then		 installBusterSourcesList
-	#elif [ "$sCurrentDistCodename" = "bullseye" ]; then	 installBookwormSourcesList; fi
+	#if [ "${sCurrentDistCodename}" = "buster" ]; then		 installBusterSourcesList
+	#elif [ "${sCurrentDistCodename}" = "bullseye" ]; then	 installBookwormSourcesList; fi
 	installNewSourcesList
 }
 
