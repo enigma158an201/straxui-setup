6fcdd84210d626f2f9c4c3467b97c2abbd2b4dcd
enigma158an201 <gwenael.goujon@gmail.com>
2024-01-06 11:21:35 +0100

    18/06/2023 update

--- include/apt-pre-instal-pkg-ubuntu.sh
+++ include/apt-pre-instal-pkg-ubuntu.sh
@@ -36,7 +36,7 @@ checkDpkgInstalled() {
 }
 aptPreinstallPkg() {
 	declare -a pkgsToInstall
-	pkgsToInstall=(net-tools wget curl tar zip grep gawk ipcalc ipv6calc git jq xinit screen cron desktop-file-utils rfkill)
+	pkgsToInstall=(net-tools wget curl tar zip grep gawk ipcalc ipv6calc git jq xinit screen cron desktop-file-utils rfkill conntrack )
 	apt-get update && apt-get upgrade
 	for pkgToInstall in "${pkgsToInstall[@]}" #${pkgsToInstall[*]}
 	do
@@ -49,6 +49,13 @@ aptPreinstallPkg() {
 aptUnbloatPkg() {
 	declare -a pkgsToRemove
 	pkgsToRemove=(sane-utils bluez evolution-data-server-common libbluetooth3 plymouth system-config-printer-common samba-common)
+	# voir pour inclure gnome-settings-daemon-common gdm3 gnome-software  (apg bolt chrome-gnome-shell fwupd fwupd-amd64-signed gdm3 gir1.2-accountsservice-1.0 gir1.2-gck-1 gir1.2-gcr-3 gir1.2-gdm-1.0 gir1.2-gnomebluetooth-3.0 gir1.2-gnomedesktop-3.0 gir1.2-grilo-0.3 gir1.2-ibus-1.0 gir1.2-mediaart-2.0
+	# gir1.2-mutter-11 gir1.2-nma-1.0 gir1.2-rsvg-2.0 gir1.2-totemplparser-1.0 gir1.2-tracker-3.0 gir1.2-upowerglib-1.0 gnome-browser-connector gnome-control-center gnome-control-center-data gnome-music gnome-remote-desktop gnome-session
+	# gnome-session-common gnome-settings-daemon gnome-settings-daemon-common gnome-shell gnome-shell-common gnome-shell-extension-prefs gnome-shell-extensions gnome-software gnome-software-common gnome-software-plugin-flatpak gnome-tweaks
+	# gstreamer1.0-pipewire ibus ibus-data ibus-gtk ibus-gtk3 ibus-gtk4 im-config libcolord-gtk4-1 libflashrom1 libfreerdp-server2-2 libftdi1-2 libfwupd2 libgcab-1.0-0 libgdm1 libgnome-bg-4-2 libgnome-bluetooth-ui-3.0-13 libgnome-rr-4-2
+	# libibus-1.0-5 libjaylink0 libjcat1 libmbim-glib4 libmbim-proxy libmutter-11-0 libnss-myhostname libqmi-glib5 libqmi-proxy libqrtr-glib0 libsmbios-c2 libsnapd-glib-2-1 libtss2-tctildr0 mutter mutter-common pipewire-alsa pipewire-audio
+	# power-profiles-daemon python3-ibus-1.0 realmd switcheroo-control
+
 	for pkgsToRemove in "${pkgsToRemove[@]}" #${pkgsToRemove[*]}
 	do
 		if [ "$(checkDpkgInstalled "$pkgToInstall")" = "true" ]; then
