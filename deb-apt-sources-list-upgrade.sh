bb8191687b7e44c958a70f5a0aa23c52ff05282e
enigma158an201 <gwenael.goujon@gmail.com>
2024-01-06 13:50:45 +0100

    replace sudo condition by suExecCommandNoPreserveEnv

--- deb-apt-sources-list-upgrade.sh
+++ deb-apt-sources-list-upgrade.sh
@@ -20,47 +20,47 @@ getDebianVersion() {
 }
 
 upgradeJessieToStretch() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        sudo sed -i 's/jessie/stretch/g' ${aptSourcesListFile}{,.d/*.list}
-        #sudo sed -i 's#/debian-security stretch/updates# stretch-security#g' ${aptSourcesListFile}
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        suExecCommandNoPreserveEnv sed -i 's/jessie/stretch/g' ${aptSourcesListFile}{,.d/*.list}
+        #suExecCommandNoPreserveEnv sed -i 's#/debian-security stretch/updates# stretch-security#g' ${aptSourcesListFile}
+    #fi
 }
 
 upgradeStretchToBuster() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        sudo sed -i 's/stretch/buster/g' ${aptSourcesListFile}{,.d/*.list}
-        #sudo sed -i 's#/debian-security buster/updates# buster-security#g' ${aptSourcesListFile} 
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        suExecCommandNoPreserveEnv sed -i 's/stretch/buster/g' ${aptSourcesListFile}{,.d/*.list}
+        #suExecCommandNoPreserveEnv sed -i 's#/debian-security buster/updates# buster-security#g' ${aptSourcesListFile} 
+    #fi
 }
 
 upgradeBusterToBullseye() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        # sudo sed -i 's/buster/bullseye/g' ${aptSourcesListFile}
-        # sudo sed -i 's/buster/bullseye/g' ${aptSourcesListFile}.d/*.list
-        sudo sed -i 's/buster/bullseye/g' ${aptSourcesListFile}{,.d/*.list}
-        sudo sed -i 's#/debian-security bullseye/updates# bullseye-security#g' ${aptSourcesListFile}
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        # suExecCommandNoPreserveEnv sed -i 's/buster/bullseye/g' ${aptSourcesListFile}
+        # suExecCommandNoPreserveEnv sed -i 's/buster/bullseye/g' ${aptSourcesListFile}.d/*.list
+        suExecCommandNoPreserveEnv sed -i 's/buster/bullseye/g' ${aptSourcesListFile}{,.d/*.list}
+        suExecCommandNoPreserveEnv sed -i 's#/debian-security bullseye/updates# bullseye-security#g' ${aptSourcesListFile}
+    #fi
 }
 
 upgradeBullseyeToBookworm() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        sudo sed -i 's/bullseye/bookworm/g' ${aptSourcesListFile}
-        sudo sed -i 's/non-free/non-free non-free-firmware/g' ${aptSourcesListFile}
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        suExecCommandNoPreserveEnv sed -i 's/bullseye/bookworm/g' ${aptSourcesListFile}
+        suExecCommandNoPreserveEnv sed -i 's/non-free/non-free non-free-firmware/g' ${aptSourcesListFile}
+    #fi
 }
 
 upgradeBookwormToTrixie() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        sudo sed -i 's/bookworm/trixie/g' ${aptSourcesListFile}
-        #sudo sed -i 's/non-free/non-free non-free-firmware/g' ${aptSourcesListFile}
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        suExecCommandNoPreserveEnv sed -i 's/bookworm/trixie/g' ${aptSourcesListFile}
+        #suExecCommandNoPreserveEnv sed -i 's/non-free/non-free non-free-firmware/g' ${aptSourcesListFile}
+    #fi
 }
 
 upgradeToSid() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        sudo sed -i 's/bookworm/sid/g' ${aptSourcesListFile}
-        #sudo sed -i 's/non-free/non-free non-free-firmware/g' ${aptSourcesListFile}
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        suExecCommandNoPreserveEnv sed -i 's/bookworm/sid/g' ${aptSourcesListFile}
+        #suExecCommandNoPreserveEnv sed -i 's/non-free/non-free non-free-firmware/g' ${aptSourcesListFile}
+    #fi
 }
 
 upgradeSourcesList() {
@@ -86,9 +86,9 @@ upgradeSourcesList() {
 }
 
 upgradeDebianDist() {
-    if command -v sudo 1>/dev/null 2>&1; then
-        sudo apt-get update && sudo apt-get upgrade && sudo apt-get full-upgrade && sudo apt-get dist-upgrade 
-    fi
+    #if command -v sudo 1>/dev/null 2>&1; then
+        suExecCommandNoPreserveEnv apt-get update && suExecCommandNoPreserveEnv apt-get upgrade && suExecCommandNoPreserveEnv apt-get full-upgrade && suExecCommandNoPreserveEnv apt-get dist-upgrade 
+    #fi
 }
 
 main() {
