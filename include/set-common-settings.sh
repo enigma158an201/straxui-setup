5bba0271b20e1863e746b3743987f3d60c6ff87f
enigma158an201 <gwenael.goujon@gmail.com>
2024-01-07 12:30:42 +0100

    update su commands

--- include/set-common-settings.sh
+++ include/set-common-settings.sh
@@ -27,37 +27,36 @@ disable-systemd-sleep() {
 	mysystemdsrc="${launchDir}$mysystemddst"
 	if [ -d "$(dirname "$mysystemddst")" ] && [ -f "$mysystemdsrc" ]; then
 		echo -e "\t>>> désactivation des mises en veille systemd"
-		suExecCommand "install -o root -g root -m 0744 -pv \"$mysystemdsrc\" \"$mysystemddst\"; \
-		systemctl daemon-reload"
+		install -o root -g root -m 0744 -pv "$mysystemdsrc" "$mysystemddst"
+		systemctl daemon-reload
 	fi
 }
 disable-wireless-connections() {
 	echo -e "\t>>> désactivation des connexions wireless"
-	suExecCommand " \
-	if (systemctl status wpa_supplicant.service 1>/dev/null); then	systemctl disable --now wpa_supplicant.service; fi; \
- 	if (command -v nmcli 1>/dev/null 2>&1); then 								nmcli radio wifi off; fi; \
-	if (command -v rfkill 1>/dev/null 2>&1); then								rfkill block wlan bluetooth; fi"
+	if (systemctl status wpa_supplicant.service 1>/dev/null); then	systemctl disable --now wpa_supplicant.service; fi
+ 	if (command -v nmcli 1>/dev/null 2>&1); then 					nmcli radio wifi off; fi
+	if (command -v rfkill 1>/dev/null 2>&1); then					rfkill block wlan bluetooth; fi
 }
 disable-cups-services() {
 	echo -e "\t>>> désactivation cups (impression)"
 	if (systemctl status cups-browsed.service 1>/dev/null) || (systemctl status cups.service 1>/dev/null || false); then
-		suExecCommand "systemctl disable --now cups-browsed.service; \
- 		systemctl disable --now cups.service"
+		systemctl disable --now cups-browsed.service
+ 		systemctl disable --now cups.service
 	fi
 }
 cronjob-disable-ipv6() {
 	echo -e "\t>>> création du job cron en cas de reactivation ipV6"
- 	if (systemctl status cron.service 1>/dev/null); then suExecCommand systemctl enable --now cron.service; fi
+ 	if (systemctl status cron.service 1>/dev/null); then systemctl enable --now cron.service; fi
 }
 set-newhostname() {
 	echo -e "\t>>> renommage de la machine suivant schéma modèle+distro"
-	suExecCommandNoPreserveEnv "${launchDir}/include/set-hostname.sh" 
+	${launchDir}/include/set-hostname.sh
 }
 main_common() {
 	source "${launchDir}/include/test-superuser-privileges.sh"
 	source "${launchDir}/include/file-edition.sh"
 	set-newhostname 		# set new host name has to be done before sshd config
-	suExecCommand sshd-config-settings
+	sshd-config-settings
 	read -rp "Désactiver les connections wifi et bluetooth? o/N"  -n 1 disableWireless
 	if [ ! "${disableWireless^^}" = "N" ] && [ ! "$disableWireless" = "" ]; then disable-wireless-connections; fi
  	disable-cups-services
