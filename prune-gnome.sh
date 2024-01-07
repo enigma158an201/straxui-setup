e179fb50c1186419419b0a5400bf283fd16e52e8
enigma158an201 <gwenael.goujon@gmail.com>
2024-01-07 09:14:50 +0100

    add network-manager and bluez to exceptions

--- prune-gnome.sh
+++ prune-gnome.sh
@@ -2,7 +2,7 @@
 
 sExceptions="^xf|^desktop-base|^libexo|^libglib|libgtop|libsoup|libstartup|^libxml|^shared-mime|^adwaita|^libcups|^network-manager"
 
-for pkg3 in gnome libreoff bluez cups
+for pkg3 in gnome libreoff cups #bluez
 do
   for pkg1 in $(dpkg -l | grep ^ii | awk {'print $2'} | grep $pkg3 | grep -vE "$sExceptions")
   do
@@ -11,10 +11,8 @@ do
   done
 done
 
-for pkg2 in $(LANG=C apt search gnome | grep "install" | grep -vE "^xf|^desktop-base|^libexo|^libglib|libgtop|libsoup|libstartup|^libxml|^shared-mime|^adwaita" | awk {'print $1'})
+for pkg2 in $(LANG=C apt search gnome | grep "install" | awk {'print $1'}) | grep -vE "$sExceptions"
 do
   echo $pkg2
   apt-get autoremove ${pkg2%%/*}
 done
-
-
