0b9f843d212b281780ca77aa7caa079a8e31148b
enigma158an201 <gwenael.goujon@gmail.com>
2024-03-31 20:02:52 +0200

    create adduser validator

---
+++ create-user-validator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# script by enigma158an201
+set -euo pipefail # set -euxo pipefail
+
+main() {
+    myUser=deposit-validator
+    sudo useradd -m -s /bin/bash "${myUser}"
+    passwd -l "${myUser}"
+    sudo usermod -aG "${myUser}"
+    su "${myUser}"
+}
+main
