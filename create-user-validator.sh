53734c927d39f08d5991e60fccc28e483bcae80a
enigma158an201 <gwenael.goujon@gmail.com>
2024-03-31 20:07:22 +0200

    add missing sudo

--- create-user-validator.sh
+++ create-user-validator.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # script by enigma158an201
-set -euo pipefail # set -euxo pipefail
+#set -euo pipefail # set -euxo pipefail
 
 createValidator() {
     myUser=deposit-validator
     sudo useradd -m -s /bin/bash "${myUser}"
-    passwd -l "${myUser}"
+    sudo passwd -l "${myUser}"
     sudo usermod -aG "${myUser}"
     su "${myUser}"
 }
