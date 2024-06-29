a846d3b677d9e4fb55a65f46483fd02408292819
enigma158an201 <gwenael.goujon@gmail.com>
2024-06-29 10:30:00 +0200

    create checkStratisEvmVersions.sh

---
+++ checkStratisEvmVersions.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+
+# script by enigma158an201
+set -euo pipefail # set -euxo pipefail
+
+# Reset
+# Color_Off='\033[0m'       # Text Reset
+normal=$(tput sgr0)
+
+# Regular Colors
+# Black='\033[0;30m'        # Black
+# Red='\033[0;31m'          # Red
+# Green='\033[0;32m'        # Green
+# Yellow='\033[0;33m'       # Yellow
+# Blue='\033[0;34m'         # Blue
+# Purple='\033[0;35m'       # Purple
+# Cyan='\033[0;36m'         # Cyan
+# White='\033[0;37m'        # White
+
+# Bold
+# BBlack='\033[1;30m'       # Black
+# BRed='\033[1;31m'         # Red
+# BGreen='\033[1;32m'       # Green
+# BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+# BPurple='\033[1;35m'      # Purple
+# BCyan='\033[1;36m'        # Cyan
+# BWhite='\033[1;37m'       # White
+
+
+bold=$(tput bold)
+
+sArch=amd64
+sSystem=linux
+sRepoFileFormat=tar.gz
+sStratisHome=/media/CommonData/telechargements/stratisEVM		#sStratisHome=$HOME/data/mainnet
+sBeaconBin=${sStratisHome}/beacon-chain							#sDepositBin=${sStratisHome}/deposit
+sGethBin=${sStratisHome}/geth
+sValidatorBin=${sStratisHome}/validator							#tLocalBins=("${sBeaconBin}" "${sGethBin}" "${sValidatorBin}")	#"${sDepositBin}"
+sProjectOwner=stratisproject
+declare -A tStratisFile=(["beacon"]="beacon" ["geth"]="geth" ["validator"]="validator")
+declare -A tStratisRepoName=(["beacon"]="prysm-stratis" ["geth"]="go-stratis" ["validator"]="prysm-stratis")
+declare -A tStratisRepoOwnerAndName=(["beacon"]="${sProjectOwner}/${tStratisRepoName[beacon]}" ["geth"]="${sProjectOwner}/${tStratisRepoName[geth]}" ["validator"]="${sProjectOwner}/${tStratisRepoName[validator]}")
+sStratisEvmUrl=https://api.github.com/repos/${sProjectOwner}/
+sBeaconUrl=${sStratisEvmUrl}${tStratisRepoName[beacon]}/releases/latest		#sStratisEvmUrl=https://api.github.com/repos/stratisproject/prysm-stratis
+sGethUrl=${sStratisEvmUrl}${tStratisRepoName[geth]}/releases/latest
+sValidatorUrl=${sStratisEvmUrl}${tStratisRepoName[validator]}/releases/latest	#sDepositUrl=${sStratisHome}/deposit
+declare -A tRepoUrl=(["beacon"]="${sBeaconUrl}" ["geth"]="${sGethUrl}" ["validator"]="${sValidatorUrl}")
+declare -A tLocalBin=(["beacon"]="${sBeaconBin}" ["geth"]="${sGethBin}" ["validator"]="${sValidatorBin}")
+
+getLocalBinVersion() {
+	sBinPath=${1}
+	eval "$sBinPath --version"
+}
+oldGitRepoBinVersion() {
+	sBinUrl=${1}
+	curl -s "$sBinUrl" | grep -i tag_name
+}
+oldGitRepoLatestBinUrl() {
+	sBinUrl=${1}
+	curl -s "$sBinUrl" | grep -i browser_download_url
+}
+gitRepoContent() {
+	sBinUrl=${1}
+	curl -s "$sBinUrl"
+}
+gitRepoBinVersion() {
+	sContent="${1}"
+	sTag=$(echo "${sContent}" | grep -i tag_name)
+	sTag="${sTag#*\: }"
+	sTag="${sTag%,*}"
+	echo "${sTag}"
+}
+gitRepoLatestBinUrl() {
+	sContent="${1}"
+	sName="${2}"
+	sUrl=$(echo "${sContent}" | grep -i browser_download_url)
+	for sCrit in "$(basename "${sName}")" ${sSystem} ${sArch} ${sRepoFileFormat}; do
+		#echo -e "$sCrit\t${sUrl}"; read -rp " "
+		sUrl="$(echo "${sUrl}" | grep "${sCrit}" || echo "false")"
+		read -rp " "
+		if [ "${sUrl}" = "false" ]; then sUrl="${sUrl#*\: }"; break; fi
+	done
+	sUrl="${sUrl#*\: }"
+	sUrl="${sUrl%,*}"
+	echo "${sUrl}"
+}
+getGhRepoReleases() {
+	sProject=${1}
+	gh release list --repo "${sProject}" #| grep --color=auto -i latest
+}
+preRequisitesInstall() {
+	if command -v sudo 1>dev/null 2>&1; then
+		if command -v apt-get 1>dev/null 2>&1; then
+			sudo apt-get update
+			if ! command -v gh 1>dev/null 2>&1; then 		sudo apt-get install gh; fi		#github-cli
+			if ! command -v curl 1>dev/null 2>&1; then 		sudo apt-get install curl; fi	#curl
+		fi		
+	fi
+	if [ -d "$HOME/.config/gh" ] || false; then 			gh auth login; fi
+}
+main() {
+	for sStratisFile in "${tStratisFile[@]}"; do #for sStratisUrl in "${tRepoUrl[@]}"; do
+		sStratisUrl="${tRepoUrl["${sStratisFile}"]}"
+		echo -e "\t>>> waiting for answer ${sStratisUrl}, please wait..."
+		sGitRepoContent="$(gitRepoContent "${sStratisUrl}")"
+		sRepoVersion=$(gitRepoBinVersion "${sGitRepoContent}")
+						#for sStratisBin in "${tLocalBins[@]}"; do
+						#	#getLocalBinVersion "${sStratisBin}"
+						#	sBinUrl="$(gitRepoLatestBinUrl "${sGitRepoContent}" "${sStratisBin}")"
+						#	if [ ! "${sBinUrl}" = "false" ]; then break; fi
+						#done
+		sBinUrl="$(gitRepoLatestBinUrl "${sGitRepoContent}" "${tLocalBin["${sStratisFile}"]}")"
+		read -rp " "
+		echo -e "${sStratisFile}\t${sStratisUrl}\t${sBinUrl}\n${sRepoVersion}\n\n" #${sStratisBin}
+	done
+}
+test() {
+	for sRepoName in "${tStratisFile[@]}"; do
+		sOwnerAndRepo="${tStratisRepoOwnerAndName[$sRepoName]}"
+		echo -e "\t>>> Getting repo ${BBlue}${sOwnerAndRepo}$normal releases for ${BBlue}${sRepoName}${normal} binary, please wait!"
+		getGhRepoReleases "${sOwnerAndRepo}" #gitRepoBinVersion ""
+	done
+}
+#preRequisitesInstall
+test
+#main
