c53c5d04e639be599597e5b76d5539374204d4bb
enigma158an201 <gwenael.goujon@gmail.com>
2024-06-30 17:48:52 +0200

    update include/test-superuser-privileges.sh

--- include/test-superuser-privileges.sh
+++ include/test-superuser-privileges.sh
@@ -103,7 +103,7 @@ suExecCommand() {
 	#elif [ ! "$EUID" = "0" ] && [ "$suQuotes" = "false" ]; then 	$sPfxSu $sCommand
 	#elif [ "$EUID" = "0" ] && [ ! "$suQuotes" = "false" ]; then 	echo "${sCommand}"
 	#elif [ "$EUID" = "0" ] && [ "$suQuotes" = "false" ]; then 		echo $sCommand
-	if [ ! "$EUID" = "0" ]; then 									eval "$sPfxSu \"${sCommand}\""
+	if [ ! "$EUID" = "0" ]; then 									eval "$sPfxSu ${sCommand}"
 	elif [ "$EUID" = "0" ] && [ ! "$suQuotes" = "false" ]; then 	eval "${sCommand}"
 	fi	
 }
