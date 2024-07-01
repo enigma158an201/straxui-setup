01013dc1d931ba05434285a52907584bb00e56f6
enigma158an201 <gwenael.goujon@gmail.com>
2024-07-01 10:01:12 +0200

    update installedString

--- prune-gnome.sh
+++ prune-gnome.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sExceptions="^xf|^desktop-base|^libexo|^libglib|libgtop|libsoup|libstartup|^libxml|^shared-mime|^adwaita|^libcups|^network-manager|^policy|^polkit|^at-spi"
-
+installedString="[installed]"
 
 pruneDpkg() {
   for pkg3 in gnome libreoff cups plymouth hitori quadrapassel sane scan transmission tumbler lynx wpasupplicant bluez
@@ -14,14 +14,14 @@ pruneDpkg() {
   done
 }
 pruneAptSearch() {
-  for pkg2 in $(LANG=C apt search gnome | grep "install" | awk '{ print $1 }' | grep -vE "$sExceptions") # apt-get search not valid
+  for pkg2 in $(LANG=C apt search gnome | grep "${installedString}" | awk '{ print $1 }' | grep -vE "$sExceptions") # apt-get search not valid
   do
     echo -e "\t>>> suppression de echo $pkg2"
     apt-get autoremove "${pkg2%%/*}"
   done
 }
 main_prune() {
-  #pruneDpkg
+  pruneDpkg
   pruneAptSearch
 }
 main_prune
