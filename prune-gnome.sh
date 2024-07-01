02c694e95de3b0f3b293852dc0b6c74c1f20da22
enigma158an201 <gwenael.goujon@gmail.com>
2024-07-01 09:57:10 +0200

    debug prune-gnome.sh

--- prune-gnome.sh
+++ prune-gnome.sh
@@ -2,17 +2,26 @@
 
 sExceptions="^xf|^desktop-base|^libexo|^libglib|libgtop|libsoup|libstartup|^libxml|^shared-mime|^adwaita|^libcups|^network-manager|^policy|^polkit|^at-spi"
 
-for pkg3 in gnome libreoff cups plymouth hitori quadrapassel sane scan transmission tumbler lynx wpasupplicant bluez
-do
-  for pkg1 in $(dpkg -l | grep ^ii | awk '{ print $2 }' | grep $pkg3 | grep -vE "$sExceptions")
-  do
-    echo -e "\t>>> suppression de $pkg1"
-    apt-get autoremove "$pkg1"
-  done
-done
 
-for pkg2 in $(LANG=C apt search gnome | grep "install" | awk '{ print $1 }' | grep -vE "$sExceptions") # apt-get search not valid
-do
-  echo -e "\t>>> suppression de echo $pkg2"
-  apt-get autoremove "${pkg2%%/*}"
-done
+pruneDpkg() {
+  for pkg3 in gnome libreoff cups plymouth hitori quadrapassel sane scan transmission tumbler lynx wpasupplicant bluez
+  do
+    for pkg1 in $(dpkg -l | grep ^ii | awk '{ print $2 }' | grep $pkg3 | grep -vE "$sExceptions")
+    do
+      echo -e "\t>>> suppression de $pkg1"
+      apt-get autoremove "$pkg1"
+    done
+  done
+}
+pruneAptSearch() {
+  for pkg2 in $(LANG=C apt search gnome | grep "install" | awk '{ print $1 }' | grep -vE "$sExceptions") # apt-get search not valid
+  do
+    echo -e "\t>>> suppression de echo $pkg2"
+    apt-get autoremove "${pkg2%%/*}"
+  done
+}
+main_prune() {
+  pruneDpkg
+  #pruneAptSearch
+}
+main_prune
