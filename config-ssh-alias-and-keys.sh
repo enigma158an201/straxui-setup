4b0f4f59f2f04767dba63a4ede012fae0a5ae808
enigma158an201 <gwenael.goujon@gmail.com>
2024-07-05 19:01:44 +0200

    update sSshPubKey list

--- config-ssh-alias-and-keys.sh
+++ config-ssh-alias-and-keys.sh
@@ -45,7 +45,7 @@ installSshKeys() {
 }
 importSshKeys() {
 	echo -e "\t>>> setup ssh keys at ${sSshLocalConf}"	#ssh-copy-id -i debian_server.pub pragmalin@debianvm
-	for sSshPubKey in "${sSshLocalConf}"/*.pub; do
+	for sSshPubKey in "${sSshRepoConf}"/*.pub; do
 		for sSshAlias in freebox-delta-wan SKY41 testsalonk wtestsalonk; do
 			if true; then 	echo "ssh-copy-id -i \"${sSshPubKey}\" \"${sSshAlias}\""; fi
 		done
