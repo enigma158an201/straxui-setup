e919ed2a155f17d7a055bce98faa11b678535a4f
enigma158an201 <gwenael.goujon@gmail.com>
2024-07-18 02:37:33 +0200

    add stopMainnetTmux function

--- home/user/bin/evm-tmux.sh
+++ home/user/bin/evm-tmux.sh
@@ -5,6 +5,8 @@ set -euo pipefail # set -euxo pipefail
 
 # to allow aliases in script as in `bash -i`
 
+if [ ! "${1:-}" = "" ]; then 	sArg=${1,,}; fi
+ 
 sTmuxSession="sky41"
 sTmuxWindow="evm"
 
@@ -17,42 +19,53 @@ else
 fi
 if ! command -v detach; then alias detach='tmux detach'; fi
 #if ! command -v attach; then alias attach="tmux -a -t $sTmuxWindow"; fi # does not work
+if ! command -v stop; then alias stop='tmux detach'; fi
 
-startMainnetTmux() {
+preCheck() {
 	if ! command -v tmux 1>/dev/null 2>&1; then 
-		echo -e "\t>>> tmux not found, please install tmux, aborting"; exit 1; 
-	else
-
-		# Check if the tmux session "${sTmuxSession}" exists, # If it doesn't exist, create a new session named "${sTmuxSession}"
-		if ! tmux has-session -t "${sTmuxSession}" 2>/dev/null; then			
-			tmux new-session -s "${sTmuxSession}" -d -x "$(tput cols)" -y "$(tput lines)"
-		fi
-		
-		if ! tmux list-windows -t "${sTmuxSession}" | grep "${sTmuxWindow}"; then
-			# Create a window named ""${sTmuxWindow}"" if not exists in the "${sTmuxSession}" session
-			tmux new-window -t "${sTmuxSession}": -n "${sTmuxWindow}" #-P 'p1'
-		fi
-
-		tmux set-option -g mouse on		#deprecated: tmux set-option -g mouse-select-pane on
-
-		# Split the window into four panes: two panes on the left and two (one higher) on the right
-		if [ "$(tmux list-panes | wc -l)" -eq "1" ] ; then
-			tmux select-pane -t 0
-			tmux split-window -h -t "${sTmuxSession}"
-			tmux select-pane -t 0 #-P 'p1'
-			tmux split-window -v -t "${sTmuxSession}" #-n 'p2'
-			tmux select-pane -t 2 #-n 'p3'
-			tmux split-window -v -l 3 #-p 90 #-t "${sTmuxSession}"
-
-			# Execute specific commands in each pane: 0 1 2 are names of panes
-			tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.0" 'tt' C-m
-			tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.1" "watch -n 1 ps aux" C-m
-			tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.2" "watch -n 1 netstat -tuln" C-m #validator
-		fi
-		# alway echo this line at right bottom
-		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.3" "echo -e 'press ctrl+b then d or enter \`detach\` to hide\n or enter \`tmux kill-session -t evm\` to kill'" C-m
-		tmux attach-session -t "${sTmuxSession}"	#tmux attach-session -t "${sTmuxSession}" -c "${sTmuxWindow}"	# Attach to the "${sTmuxSession}" session
+		echo -e "\t>>> tmux not found, please install tmux, aborting"; 
+		exit 1
 	fi
+}
+stopMainnetTmux() {
+	for iWindow in 4 3 2 1; do
+	if [ "$(tmux list-panes | wc -l)" -eq "$iWindow" ] ; then 	
+		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.$((iWindow - 1))" C-c
+		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.$((iWindow - 1))" 'exit' C-m
+	fi
+	done
+}
+startMainnetTmux() {
+	# Check if the tmux session "${sTmuxSession}" exists, # If it doesn't exist, create a new session named "${sTmuxSession}"
+	if ! tmux has-session -t "${sTmuxSession}" 2>/dev/null; then			
+		tmux new-session -s "${sTmuxSession}" -d -x "$(tput cols)" -y "$(tput lines)"
+	fi
+		
+	if ! tmux list-windows -t "${sTmuxSession}" | grep "${sTmuxWindow}"; then
+		# Create a window named ""${sTmuxWindow}"" if not exists in the "${sTmuxSession}" session
+		tmux new-window -t "${sTmuxSession}": -n "${sTmuxWindow}" #-P 'p1'
+	fi
+
+	tmux set-option -g mouse on		#deprecated: tmux set-option -g mouse-select-pane on
+
+	# Split the window into four panes: two panes on the left and two (one higher) on the right
+	if [ "$(tmux list-panes | wc -l)" -eq "1" ] ; then
+		tmux select-pane -t 0
+		tmux split-window -h -t "${sTmuxSession}"
+		tmux select-pane -t 0 #-P 'p1'
+		tmux split-window -v -t "${sTmuxSession}" #-n 'p2'
+		tmux select-pane -t 2 #-n 'p3'
+		tmux split-window -v -l 3 #-p 90 #-t "${sTmuxSession}"
+
+		# Execute specific commands in each pane: 0 1 2 are names of panes
+		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.0" 'tt' C-m
+		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.1" "watch -n 1 ps aux" C-m
+		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.2" "watch -n 1 netstat -tuln" C-m #validator
+	fi
+	# alway echo this line at right bottom
+	tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.3" "echo -e 'press ctrl+b then d or enter \`detach\` to hide\n or enter \`tmux kill-session -t evm\` to kill'" C-m
+	tmux attach-session -t "${sTmuxSession}"	#tmux attach-session -t "${sTmuxSession}" -c "${sTmuxWindow}"	# Attach to the "${sTmuxSession}" session
+
 }
 upgradeBinTmuxEvmScript() {
 	if command -v evm-tmux.sh 1>/dev/null 2>&1; then
@@ -69,7 +82,10 @@ upgradeBinTmuxEvmScript() {
 	fi
 }
 main_evm() {
-	upgradeBinTmuxEvmScript # 1st find git source, and locally installed script, then upgrade if necessary
-	startMainnetTmux 		# then start new script
+	preCheck
+	if [ "${sArg:-}" = "upgrade" ] || [ "${sArg:-}" = "update" ]; then 	upgradeBinTmuxEvmScript # 1st find git source, and locally installed script, then upgrade if necessary
+	elif [ "${sArg:-}" = "start" ] || [ "${sArg:-}" = "" ]; then 		startMainnetTmux 		# start script
+	elif [ "${sArg:-}" = "stop" ] || [ "${sArg:-}" = "kill" ]; then 	stopMainnetTmux 		# stop script
+	fi
 }
 main_evm
