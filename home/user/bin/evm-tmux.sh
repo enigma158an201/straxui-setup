bad970cf59bf1ac9f77aa2fd794c69af1bcdf896
enigma158an201 <gwenael.goujon@gmail.com>
2024-07-19 11:52:43 +0200

    update msg instructions to kill tmux session

--- home/user/bin/evm-tmux.sh
+++ home/user/bin/evm-tmux.sh
@@ -89,7 +89,7 @@ startMainnetTmux() {
 	fi
 	# alway echo this line at right bottom
 	if [ "${TMUX_PANE}" = "%4" ]; then
-		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.3" "echo -e 'to hide tmux (and keep running): press ctrl+b then d or enter \`tmux detach\`\nor to kill tmux: enter \`tmux kill-session -t evm\` to kill'" C-m
+		tmux send-keys -t "${sTmuxSession}:${sTmuxWindow}.3" "echo -e 'to hide tmux (and keep running): press ctrl+b then d or enter \`tmux detach\`\nor to kill tmux: enter \`tmux kill-session -t sky41\` to kill'" C-m
 	fi
 	tmux attach-session -t "${sTmuxSession}"	#tmux attach-session -t "${sTmuxSession}" -c "${sTmuxWindow}"	# Attach to the "${sTmuxSession}" session
 }
