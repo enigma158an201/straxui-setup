df864816c70623d5171a5cc68a3ff450faf5a643
enigma158an201 <gwenael.goujon@gmail.com>
2024-07-19 14:01:08 +0200

    add case insentive for usename

--- home/user/bin/evm-tmux.sh
+++ home/user/bin/evm-tmux.sh
@@ -11,11 +11,11 @@ if [ ! "${1:-}" = "" ]; then 	sArg=${1,,}; fi
 sTmuxSession="sky41"
 sTmuxWindow="evm"
 sUsername=$(whoami)
-if [[ "${sUsername}" =~ "sky " ]]; then
+if [[ "${sUsername,,}" =~ "sky " ]]; then
 	sAlias1="mainnet-geth"
 	sAlias2="mainnet-prysm"
 	sAlias3="mainnet-validator-run"
-elif [[ "${sUsername}" =~ "gwen" ]] || [[ "${sUsername}" =~ "freebox" ]]; then
+elif [[ "${sUsername,,}" =~ "gwen" ]] || [[ "${sUsername,,}" =~ "freebox" ]]; then
 	sAlias1="watch -n 1 ss -tp"
 	sAlias2="watch -n 1 ps aux"
 	sAlias3="watch -n 1 netstat -tulanp"
