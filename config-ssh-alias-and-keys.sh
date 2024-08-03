a02bb9a697900237bb75b1c757f6e37fe47a3072
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-04 00:27:50 +0200

    fix source config-ssh-alias-and-keys.sh

--- config-ssh-alias-and-keys.sh
+++ config-ssh-alias-and-keys.sh
@@ -6,7 +6,7 @@ set -euo pipefail # set -euxo pipefail
 launchDir="$(dirname "$0")"
 if [ "${launchDir}" = "." ]; then launchDir="$(pwd)"; elif [ "${launchDir}" = "include" ]; then eval launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"
 source "${launchDir}/include/test-superuser-privileges.sh"
-source "${launchDir}/include/set-common-settings.sh"
+#source "${launchDir}/include/set-common-settings.sh"
 
 sSshSubFolder=.ssh
 sSshAliasConfig=${sSshSubFolder}/config
@@ -59,6 +59,7 @@ importSshKeys() {
 	#done
 }
 main_ssh_config() {
+	sudo bash -c "source ${launchDir}/include/set-common-settings.sh"
 	installSshAlias
 	#installSshKeys
 	#importSshKeys
