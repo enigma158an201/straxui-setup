36dbdffcfb6b897cfb391ff1f5e8b120a5ec1306
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-03 22:39:47 +0200

    update include/check-echo-before-suexec.sh

--- include/check-echo-before-suexec.sh
+++ include/check-echo-before-suexec.sh
@@ -6,6 +6,6 @@
 #disable-ip6.sh strax-mn-install-ubuntu.sh update-or-install-strax-wallet-deb-bullseye.sh switch-from-iptables-to-nftables-setup-sky.sh check-echo-before-suexec.sh strax-mn-start.sh install-strax-wallet-gz.sh
 # test-superuser-privileges.sh set-common-settings.sh pre-install-pkgs.sh set-ssh-nonroot-user-keys.sh get-network-settings.sh
 for myFile in apt-install-cmd.sh file-edition.sh set-grub-kernel-parameter.sh apt-pre-instal-pkg-ubuntu.sh set-hostname.sh disable-systemd-sleep.sh dpkg-install-cmd.sh; do
- 	grep --color -inH 'echo -e' $myFile
- 	grep --color -inH 'suexec' $myFile
+ 	grep --color -inH 'echo -e' ${myFile}
+ 	grep --color -inH 'suexec' ${myFile}
 done
