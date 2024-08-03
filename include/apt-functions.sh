8c43d114ffd2cd09c4a7cf3de58418951404fecb
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-03 22:56:11 +0200

    update include/apt-functions.sh

--- include/apt-functions.sh
+++ include/apt-functions.sh
@@ -6,22 +6,22 @@ getDpkgListInstalled() {
 	#declare -a dpkgInstalled
 	dpkgGlobList="$(dpkg -l | tail -n +6 | awk '{ print $2 }')"
 	prefix="ii  " # to do: check if other prefix can return installed software (ie: for update purpose)
-	dpkgInstalled="${dpkgGlobList[*]//$prefix/}"
+	dpkgInstalled="${dpkgGlobList[*]//${prefix}/}"
 	echo "${dpkgInstalled[*]}"
 }
 checkDpkgInstalledTests() {
 	#dpkg -l | grep -E '(^|\s+)cron\b'
 	#dpkgInstalledList="$(getDpkgListInstalled)"			# | grep cron
 	#pkgNamePrefix="$1"
-	#sPkgInstalled="${dpkgInstalledList[@]//$pkgNamePrefix/}"
+	#sPkgInstalled="${dpkgInstalledList[@]//${pkgNamePrefix}/}"
 	
 	#array=(elem1/a elem1/b elem2/a elem2/b)
 	#prefix=elem1
 	#sPkgInstalled=()
 	#for element in ${dpkgInstalledList[@]}; do
-		#[[ $element == $pkgNamePrefix/* ]] && sPkgInstalled+=("$element")
-		#echo "$element ";read -rp " "
-		#[[ $element == $pkgNamePrefix ]] && sPkgInstalled+=("$element")
+		#[[ ${element} == ${pkgNamePrefix}/* ]] && sPkgInstalled+=("${element}")
+		#echo "${element} ";read -rp " "
+		#[[ ${element} == ${pkgNamePrefix} ]] && sPkgInstalled+=("${element}")
 	#done
 	#if [ -z "${sPkgInstalled[@]}" ]; then echo "false"; exit 1; else echo "true"; exit 0; fi
 	#unset sPkgInstalled
@@ -39,10 +39,10 @@ checkDpkgInstalledTests() {
 checkDpkgInstalled() {
 	pkgname="$1"
 	#noPackageFoundString="no packages found matching"
-	#result="$(dpkg-query --show "$pkgname" && echo "true") || echo "false")"
-	#result="$(LANG=C /usr/bin/dpkg-query --show --showformat='\${db:Status-Status}\n' "$pkgname")"
-	#if [[ $result =~ \$not-installed ]] || [[ $result =~ $noPackageFoundString ]]; then echo "false"; else echo "true"; fi 
+	#result="$(dpkg-query --show "${pkgname}" && echo "true") || echo "false")"
+	#result="$(LANG=C /usr/bin/dpkg-query --show --showformat='\${db:Status-Status}\n' "${pkgname}")"
+	#if [[ ${result} =~ \${not-installed} ]] || [[ ${result} =~ ${noPackageFoundString} ]]; then echo "false"; else echo "true"; fi 
 	installedString="[installed]"
-	result="$(LANG=C /usr/bin/apt search --names-only ^"$pkgname"$ | grep -Ew "$pkgname|$installedString")" # apt-get search not valid
-	if [[ $result =~ $installedString ]]; then echo "true"; else echo "false"; fi 
+	result="$(LANG=C /usr/bin/apt search --names-only ^"${pkgname}"$ | grep -Ew "${pkgname}|${installedString}")" # apt-get search not valid
+	if [[ ${result} =~ ${installedString} ]]; then echo "true"; else echo "false"; fi 
 }
