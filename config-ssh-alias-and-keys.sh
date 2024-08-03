eb37405a8a459955d2fe75517c03bda0fb28c94c
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-04 01:02:24 +0200

    fix config-ssh-alias-and-keys.sh

--- config-ssh-alias-and-keys.sh
+++ config-ssh-alias-and-keys.sh
@@ -63,7 +63,7 @@ updateSshdConfig() {
 	suExecCommand "bash -c 'for sSshdConfigFile in ${launchDir}/etc/sshd_config.d/*.conf; do
 		sSshdConfigDst=/etc/ssh/sshd_config.d/\$sSshdConfigFile
 		sSshdConfigSrc=${launchDir}\$sSshdConfigDst
-		if [ -d $\(dirname \"\$sSshdConfigDst\"\) ] && [ -f \"\$sSshdConfigSrc\" ]; then
+		if [ -d \$(dirname \"\$sSshdConfigDst\"\) ] && [ -f \"\$sSshdConfigSrc\" ]; then
 			install -o root -g root -m 0744 -pv \$sSshdConfigSrc \$sSshdConfigDst
 		fi
 	done
