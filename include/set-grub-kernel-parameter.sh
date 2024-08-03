833b62acc6bbcf6766ac488424262a2758d718e6
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-03 23:35:44 +0200

    update include/set-grub-kernel-parameter.sh

--- include/set-grub-kernel-parameter.sh
+++ include/set-grub-kernel-parameter.sh
@@ -11,13 +11,13 @@ grubUpdate() {
 main_grubsettings() {
  	#sCommand="sed -i '/GRUB_CMDLINE_LINUX/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub"
  	#sCommandDefault="sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub"
-	##echo -e "$sCommand \n $sCommandDefault"
-	#$sCommand
- 	#$sCommandDefault 			#sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub then #sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ ipv6.disable=1"/' /etc/default/grub
+	##echo -e "${sCommand} \n ${sCommandDefault}"
+	#${sCommand}
+ 	#${sCommandDefault} 			#sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub then #sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ ipv6.disable=1"/' /etc/default/grub
 	bDisabledIpV6="$(grep ^GRUB_CMDLINE_LINUX= /etc/default/grub | grep ipv6.disable || echo "false")"
 	bDisabledDefaultIpV6="$(grep ^GRUB_CMDLINE_LINUX_DEFAULT= /etc/default/grub | grep ipv6.disable || echo "false")"
-	if [ "$bDisabledIpV6" = "false" ]; then				sed -i '/GRUB_CMDLINE_LINUX=/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub; fi
- 	if [ "$bDisabledDefaultIpV6" = "false" ]; then		sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub; fi
+	if [ "${bDisabledIpV6}" = "false" ]; then				sed -i '/GRUB_CMDLINE_LINUX=/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub; fi
+ 	if [ "${bDisabledDefaultIpV6}" = "false" ]; then		sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ ipv6.disable=1\"/' /etc/default/grub; fi
 	grubUpdate
 }
 main_grubsettings
