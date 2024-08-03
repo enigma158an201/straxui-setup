21d02e2906958c04c709ada65bedfc2be6350c89
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-03 23:34:03 +0200

    update include/set-common-settings.sh

--- include/set-common-settings.sh
+++ include/set-common-settings.sh
@@ -4,7 +4,7 @@ set -euo pipefail #; set -x
 
 # this script requires super user privileges and do not contain any suExec
 launchDir="$(dirname "$0")"
-if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; elif [ "$launchDir" = "include" ]; then eval launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"
+if [ "${launchDir}" = "." ]; then launchDir="$(pwd)"; elif [ "${launchDir}" = "include" ]; then eval launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"
 #source "${launchDir}/include/test-superuser-privileges.sh"
 source "${launchDir}/include/file-edition.sh"
 
@@ -12,10 +12,10 @@ sshd-config-settings() {
 	echo -e "\t>>> application des fichiers config sshd"
 	#for sSshdConfigFile in enable-only-ip4.conf prohibit-root.conf pubkey-only.conf pubkey-accepted-types.conf sshd-port.conf; do
 	for sSshdConfigFile in "${launchDir}"/etc/sshd_config.d/*.conf; do
-		sSshdConfigDst="/etc/ssh/sshd_config.d/$sSshdConfigFile"
-		sSshdConfigSrc="${launchDir}$sSshdConfigDst"
-		if [ -d "$(dirname "$sSshdConfigDst")" ] && [ -f "$sSshdConfigSrc" ]; then 
-			install -o root -g root -m 0744 -pv "$sSshdConfigSrc" "$sSshdConfigDst"
+		sSshdConfigDst="/etc/ssh/sshd_config.d/${sSshdConfigFile}"
+		sSshdConfigSrc="${launchDir}${sSshdConfigDst}"
+		if [ -d "$(dirname "${sSshdConfigDst}")" ] && [ -f "${sSshdConfigSrc}" ]; then 
+			install -o root -g root -m 0744 -pv "${sSshdConfigSrc}" "${sSshdConfigDst}"
 		fi
 	done
 	systemctl restart sshd.service
@@ -25,12 +25,12 @@ disable-systemd-sleep() {
 	#AllowHibernation=yes			to	AllowHibernation=no
 	#AllowSuspendThenHibernate=yes	to	AllowSuspendThenHibernate=no
 	#AllowHybridSleep=yes			to	AllowHybridSleep=no
-	#suExecCommand "bash -c \"$launchDir/include/disable-systemd-sleep.sh\""
+	#suExecCommand "bash -c \"${launchDir}/include/disable-systemd-sleep.sh\""
 	sSystemdSleepDst="/etc/systemd/sleep.conf"
-	sSystemdSleepSrc="${launchDir}$sSystemdSleepDst"
-	if [ -d "$(dirname "$sSystemdSleepDst")" ] && [ -f "$sSystemdSleepSrc" ]; then
+	sSystemdSleepSrc="${launchDir}${sSystemdSleepDst}"
+	if [ -d "$(dirname "${sSystemdSleepDst}")" ] && [ -f "${sSystemdSleepSrc}" ]; then
 		echo -e "\t>>> désactivation des mises en veille systemd"
-		install -o root -g root -m 0744 -pv "$sSystemdSleepSrc" "$sSystemdSleepDst"
+		install -o root -g root -m 0744 -pv "${sSystemdSleepSrc}" "${sSystemdSleepDst}"
 		systemctl daemon-reload
 	fi
 }
@@ -60,9 +60,9 @@ main_common() {
 	whoami
 	#set-newhostnam || true		# set new host name has to be done before sshd config
 	echo -e "\t>>> initialisation des paramètres du serveur ssh"
-	if command -v sshd 1>/dev/null 2>&1; then 										sshd-config-settings; fi
+	if command -v sshd 1>/dev/null 2>&1; then 											sshd-config-settings; fi
 	read -rp "Désactiver les connections wifi et bluetooth? o/N"  -n 1 sDisableWireless
-	if [ ! "${sDisableWireless^^}" = "N" ] && [ ! "$sDisableWireless" = "" ]; then 	disable-wireless-connections; fi
+	if [ ! "${sDisableWireless^^}" = "N" ] && [ ! "${sDisableWireless}" = "" ]; then 	disable-wireless-connections; fi
 	echo -e "\t>>> désactivation de cups"
 	disable-cups-services
 	echo -e "\t>>> désactivation de systemd-sleep"
