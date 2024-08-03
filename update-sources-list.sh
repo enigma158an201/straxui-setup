46c42f78398d470a33611981ec61e162533f57c8
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-03 22:15:10 +0200

    update update-sources-list.sh

--- update-sources-list.sh
+++ update-sources-list.sh
@@ -9,12 +9,12 @@ if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; elif [ "$launchDir" = "inclu
 source "${launchDir}/include/test-superuser-privileges.sh"
 
 getInstalledDebianDistCodeName() {
-	myetcosrelease=/etc/os-release
-	if [ -f "${myetcosrelease}" ]; then
-		sOsId="$(grep -i ^ID= "${myetcosrelease}")"
+	sEtcOsrelease=/etc/os-release
+	if [ -f "${sEtcOsrelease}" ]; then
+		sOsId="$(grep -i ^ID= "${sEtcOsrelease}")"
 		sOsId="${sOsId##*=}"
 		if [ "$sOsId" = "debian" ]; then 
-			sOsRelease="$(grep -i ^VERSION_CODENAME= "${myetcosrelease}")"
+			sOsRelease="$(grep -i ^VERSION_CODENAME= "${sEtcOsrelease}")"
 			sOsRelease="${sOsRelease##*=}"
 			echo "${sOsRelease}"
 		else
@@ -27,14 +27,15 @@ getInstalledDebianDistCodeName() {
 sCurrentDistCodename="$(getInstalledDebianDistCodeName)"
 
 installNewSourcesList() {
-	mysourceslistdst="/etc/apt/sources.list"
-	if [ "${sCurrentDistCodename}" = "buster" ]; then			mysourceslistsrc="${launchDir}/etc/apt/bullseye-sources.list"
-	elif [ "${sCurrentDistCodename}" = "bullseye" ]; then		mysourceslistsrc="${launchDir}/etc/apt/bookworm-sources.list"
-	elif [ "${sCurrentDistCodename}" = "bookworm" ]; then		mysourceslistsrc="${launchDir}/etc/apt/bookworm-sources.list"
+	sSourcesListDst="/etc/apt/sources.list"
+	if [ "${sCurrentDistCodename}" = "buster" ]; then			sSourcesListSrc="${launchDir}/etc/apt/bullseye-sources.list"
+	elif [ "${sCurrentDistCodename}" = "bullseye" ]; then		sSourcesListSrc="${launchDir}/etc/apt/bookworm-sources.list"
+	elif [ "${sCurrentDistCodename}" = "bookworm" ]; then		sSourcesListSrc="${launchDir}/etc/apt/trixie-sources.list"
+	elif [ "${sCurrentDistCodename}" = "trixie" ]; then			sSourcesListSrc="${launchDir}/etc/apt/forky-sources.list"
 	fi
-	mysourceslistbak="${mysourceslistdst}.${sCurrentDistCodename}-gg.save"
-	if [ -f "${mysourceslistdst}" ]; then
-		sResultDiff="$(diff "${mysourceslistsrc}" "${mysourceslistdst}")"
+	sSourcesListBak="${sSourcesListDst}.${sCurrentDistCodename}-gg.save"
+	if [ -f "${sSourcesListDst}" ]; then
+		sResultDiff="$(diff "${sSourcesListSrc}" "${sSourcesListDst}")"
 		if [ "${sResultDiff}" = "" ]; then bDiffApt="false"
 		else bDiffApt="true"
 		fi 	
@@ -45,26 +46,26 @@ installNewSourcesList() {
 	else confirmOverwriteRepo="N"; fi
 	if [ "${confirmOverwriteRepo^^}" = "O" ] || [ "${bDiffApt}" = "true" ]; then
 		
-		echo -e "\t>>> sauvegarde du fichier ${mysourceslistdst} pour Debian ${sCurrentDistCodename} dans ${mysourceslistbak}"
-		if [ -f "${mysourceslistdst}" ]; then 
-			suExecCommand mv -i "${mysourceslistdst}" "${mysourceslistbak}"	
+		echo -e "\t>>> sauvegarde du fichier ${sSourcesListDst} pour Debian ${sCurrentDistCodename} dans ${sSourcesListBak}"
+		if [ -f "${sSourcesListDst}" ]; then 
+			suExecCommand mv -i "${sSourcesListDst}" "${sSourcesListBak}"	
 		fi
-		echo -e "\t>>> installation de la version suivante pour ${mysourceslistdst}"
-		suExecCommand install -o root -g root -m 0744 -pv "${mysourceslistsrc}" "${mysourceslistdst}"
+		echo -e "\t>>> installation de la version suivante pour ${sSourcesListDst}"
+		suExecCommand install -o root -g root -m 0744 -pv "${sSourcesListSrc}" "${sSourcesListDst}"
 		if true; then 
-			echo -e "\t>>> nouvelle version de ${mysourceslistdst} installee avec succès \
+			echo -e "\t>>> nouvelle version de ${sSourcesListDst} installee avec succès \
 			\n\t>>> pour installer les nouveaux paquets, lancer en root (avec sudo ou su - ET sans les crochets) :\
 			\n \t # [sudo] apt update && [sudo] apt dist-upgrade "
 		fi
 	elif [ "${bDiffApt}" = "false" ]; then
-		echo -e "\t>>> pas de modification pour ${mysourceslistsrc} \
+		echo -e "\t>>> pas de modification pour ${sSourcesListSrc} \
 		\n\t>>> pour installer les nouveaux paquets, lancer en root (avec sudo ou su - ET sans les crochets) :\
 		\n\t # [sudo] apt update && [sudo] apt dist-upgrade "
 	else
-		echo -e "\t>>> abandon de l'installation de la version suivante pour ${mysourceslistsrc}"
+		echo -e "\t>>> abandon de l'installation de la version suivante pour ${sSourcesListSrc}"
 		exit 1
 	fi
-	unset mysourceslist{dst,src,bak}
+	unset sSourcesList{Dst,Src,Bak}
 }
 
 main() {
