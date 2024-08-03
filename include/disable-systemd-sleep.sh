2e1d5416711f2d7d115b63c4d9b2cbaff5cad03d
enigma158an201 <gwenael.goujon@gmail.com>
2024-08-03 23:14:26 +0200

    update include/disable-systemd-sleep.sh

--- include/disable-systemd-sleep.sh
+++ include/disable-systemd-sleep.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail #; set -x
 launchDir="$(dirname "$0")"
-if [ "$launchDir" = "." ]; then launchDir="$(pwd)"; elif [ "$launchDir" = "include" ]; then eval launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"; launchDir="${launchDir//\/\//}"
+if [ "${launchDir}" = "." ]; then launchDir="$(pwd)"; elif [ "${launchDir}" = "include" ]; then eval launchDir="$(pwd)"; fi; launchDir="${launchDir//include/}"; launchDir="${launchDir//\/\//}"
 
 main_disable_sleep() {
  	source "${launchDir}/include/file-edition.sh"
@@ -11,7 +11,7 @@ main_disable_sleep() {
 	for sleepLine in ${sleepLines}; do
 		lineWithoutVal="${sleepLine/yes/}"
 		lineWithoutVal="${sleepLine/no/}"
-		read -rp "$sleepLine"
+		#read -rp "${sleepLine}"
 		uncomment			"${lineWithoutVal}"	"${sleepconfDir}"
 		lineNo="${lineWithoutVal}no"
 		setParameterInFile "${sleepconfDir}"	"${lineWithoutVal}"		"${lineNo}"
